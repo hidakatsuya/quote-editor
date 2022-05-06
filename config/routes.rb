a3fd377a8237340e6107df549b9885fb0d5d0d01
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-05-06 16:23:16 +0900

    add authorization and a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
 
-  # Defines the root path route ("/")
-  root "quotes#index"
+  devise_for :users
   resources :quotes
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,7 @@ require "test_helper"
 require "playwright_driver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Warden::Test::Helpers
+
   driven_by :playwright, screen_size: [1400, 1400]
 end
