7c82803c8d5f6a7b9c22ca24e988952ddf9efff8
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-05-12 00:25:25 +0900

    add line_item_date model and its CRUD controller without Turbo

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
 
   devise_for :users
-  resources :quotes
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
