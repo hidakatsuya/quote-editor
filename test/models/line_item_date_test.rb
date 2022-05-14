486c2c6b80d2b8582120a657fcbe1671d9f315f1
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-05-14 16:22:42 +0900

    add Turbo Frames and Turbo Streams to  the line item dates controller

--- test/models/line_item_date_test.rb
+++ test/models/line_item_date_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class LineItemDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#previous_date" do
+    assert_equal line_item_dates(:today), line_item_dates(:next_week).previous_date
+    assert_nil line_item_dates(:today).previous_date
+  end
 end
